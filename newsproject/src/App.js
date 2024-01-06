b49591740a148b8d060debc0f37bbdea2ebad670
Taufeek09 <140899612+Taufeek09@users.noreply.github.com>
2024-01-06 02:33:26 +0000

    send

--- newsproject/src/App.js
+++ newsproject/src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
+import News from './components/News';
+import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
   render() {
     return (
+      <>
       <div>
         <Navbar />
+        <News />
+        <NewsItem />
       </div>
+      </>
     )
   }
 }
