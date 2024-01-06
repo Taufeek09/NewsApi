e28059765504d6cbd9ca2e59f301be9f27cd00f2
Taufeek09 <140899612+Taufeek09@users.noreply.github.com>
2024-01-06 11:23:17 +0000

    SEND

--- newsproject/src/components/Navbar.js
+++ newsproject/src/components/Navbar.js
@@ -6,7 +6,7 @@ export class Navbar extends Component {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            NewsMonkey
+          <strong> NewsMonkey</strong> 
           </a>
           <button
             className="navbar-toggler"
@@ -22,12 +22,12 @@ export class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#/">
+                <a className="nav-link active fw-bold" aria-current="page" href="#/">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <a className="nav-link fw-bold" href="/about">
                   About
                 </a>
               </li>
--- newsproject/src/components/News.js
+++ newsproject/src/components/News.js
@@ -5,8 +5,7 @@ export class News extends Component {
   render() {
     return (
       <div>
-        This is News Component.
-        <NewsItem />
+        <NewsItem title="My News" discripation="my dicprication" imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCPMsCoOirsXgCLEZy1csq7SmBhJkQaVRbHA&usqp=CAU"/>
       </div>
     )
   }
