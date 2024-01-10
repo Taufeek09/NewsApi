b72e861fc290524e6efb1de71643c810cca31471
Taufeek09 <140899612+Taufeek09@users.noreply.github.com>
2024-01-10 05:44:33 +0000

    Changed the API call to fetch top headlines from India instead of the US. Added a fallback image if the API does not return an image URL.

--- newsproject/src/components/NewsItem.js
+++ newsproject/src/components/NewsItem.js
@@ -10,9 +10,9 @@ export class NewsItem extends Component {
       <div className="container">
        
       <div className="my-3">
-        {/* GET https://newsapi.org/v2/top-headlines?country=us&apiKey=a13856cb8c664403a76485028be0f622 */}
+        {/* GET https://newsapi.org/v2/top-headlines?country=in&apiKey=a13856cb8c664403a76485028be0f622 */}
         <div className="card" style={{width: "18rem"}}>
-          <img src={imgUrl} className="card-img-top" alt="..."/>
+          <img src={<img src={!imgUrl?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUz85hA0ErDmm3y6XFS4w5JBNW6m9ajupJ_Q&usqp=CAU":imgUrl} className="card-img-top" alt="..."/>} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title} ...</h5>
               <p className="card-text">{discripation} .....</p>
